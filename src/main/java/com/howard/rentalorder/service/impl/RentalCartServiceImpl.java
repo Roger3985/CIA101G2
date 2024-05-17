abd3f875f8478edad09d652b8325176b7b1d86af
CYH <howardcyh@icloud.com>
2024-05-17 13:38:09 +0800

    實作後台功能 - 把出貨功能銜接上後台、優化創建訂單過程之資料庫互動(若a跟b購物車裡都有編號5001之商品，而a先結帳，則b的redis裡面的商品資料的租借品狀態也要同步變成1(已預約))、部分結帳(已被租的商品不能被結帳)、優化前後台使用者介面

--- src/main/java/com/howard/rentalorder/service/impl/RentalCartServiceImpl.java
+++ src/main/java/com/howard/rentalorder/service/impl/RentalCartServiceImpl.java
@@ -34,7 +34,7 @@ public class RentalCartServiceImpl implements RentalCartService {
 
         try (Jedis jedis = jedisPool.getResource()) {
 
-            jedis.select(1);
+            jedis.select(10);
             String cursor = "0";
 
             do {
@@ -71,9 +71,35 @@ public class RentalCartServiceImpl implements RentalCartService {
 
         try (Jedis jedis = jedisPool.getResource()) {
 
-            jedis.select(1);
+            jedis.select(10);
             String itemKey = "member : " + memNo + " : cartItem : " + map.get("rentalNo");
             jedis.hmset(itemKey, map);
+
+            // 測試 redis 同步更新
+//            Map<String, String> map1 = new HashMap<>();
+//            map1.put("rentalNo", String.valueOf(5001));
+//            map1.put("rentalCatNo", String.valueOf(1));
+//            map1.put("rentalName", "格紋成套西裝");
+//            map1.put("rentalPrice", String.valueOf(15500));
+//            map1.put("rentalDesPrice", String.valueOf(4000));
+//            map1.put("rentalSize", String.valueOf(2));
+//            map1.put("rentalColor", "黑色");
+//            map1.put("rentalInfo", "款式：劍領 單排釦。適用場合：婚攝 婚宴 宴客 求婚");
+//            map1.put("rentalStat", String.valueOf(0));
+//            jedis.hmset("member : 2 : cartItem : 5001", map1);
+//
+//            Map<String, String> map2 = new HashMap<>();
+//            map2.put("rentalNo", String.valueOf(5011));
+//            map2.put("rentalCatNo", String.valueOf(1));
+//            map2.put("rentalName", "雙排扣格紋成套西裝");
+//            map2.put("rentalPrice", String.valueOf(12500));
+//            map2.put("rentalDesPrice", String.valueOf(4000));
+//            map2.put("rentalSize", String.valueOf(3));
+//            map2.put("rentalColor", "深卡其色");
+//            map2.put("rentalInfo", "款式：標準領 單排釦 雙扣。適用場合：婚攝 婚宴 求婚 日常");
+//            map2.put("rentalStat", String.valueOf(0));
+//            jedis.hmset("member : 2 : cartItem : 5011", map2);
+
 
         }
 
@@ -83,7 +109,7 @@ public class RentalCartServiceImpl implements RentalCartService {
             // 從連線池取得連線物件
         try(Jedis jedis = jedisPool.getResource()) {
             // 使用 1 號倉庫
-            jedis.select(1);
+            jedis.select(10);
            /*
             * 把要刪除的商品編號轉化成 Redis 裡儲存的 key 的格式，說明如下 :
             * 用 stream() 從 rentalNos 得到 stream 物件 -> 用 map() 遍歷 stream 物件中的各個元素，並執行以下操作 :
@@ -96,8 +122,41 @@ public class RentalCartServiceImpl implements RentalCartService {
             // 會返回刪除成功的筆數，還沒用到但先寫著備用
             long result = jedis.del(keys);
 
-        }
+        } // try 結束
 
     } // deleteFromCart 方法結束
 
-}
+    public void updateCart(List<Integer> rentalNos, String field, String value) {
+
+        try(Jedis jedis = jedisPool.getResource()) {
+
+            jedis.select(10);
+            String cursor = "0";
+
+            for (Integer rentalNo : rentalNos) {
+
+                do {
+                    // 設定掃描參數
+                    ScanParams scanParams = new ScanParams().match("member : * : cartItem : " + rentalNo).count(100);
+                    // 執行掃描，得到裝著所有符合條件的 key 和 新的 cursor 值的 List
+                    ScanResult<String> scanResult = jedis.scan(cursor, scanParams);
+                    // 得到裝著所有符合條件的 key 的 List
+                    List<String> keys = scanResult.getResult();
+                    // 對所有符合格式的 key，裡面的 map 裡面的 value 做更新
+                    for (String key : keys) {
+                        jedis.hset(key, field, value);
+                    }
+                    // 得到新的 cursor 值
+                    cursor = scanResult.getCursor();
+
+                } while (!cursor.equals("0"));
+
+            } // forEach 結束
+
+        } // try 結束
+
+    } // updateCart 方法結束
+
+
+
+} // class 結束
--- src/main/java/com/howard/rentalorder/service/impl/RentalOrderShippingService.java
+++ src/main/java/com/howard/rentalorder/service/impl/RentalOrderShippingService.java
@@ -20,32 +20,30 @@ public class RentalOrderShippingService {
     @Autowired
     private RentalOrderServiceImpl orderService;
 
-    public String shipping() { // 出貨
-
+    public String shipping(Integer rentalOrdNo) { // 出貨
 
         Map<String, Object> map = new HashMap<>();
-        map.put("rentalOrdNo", 13);
-        List<RentalOrder> rentalOrderList = orderService.getByAttributes(map);
-        RentalOrder rentalOrder = rentalOrderList.get(0);
+        map.put("rentalOrdNo", rentalOrdNo);
+        RentalOrder rentalOrder = orderService.getByAttributes(map).get(0);
         AllInOne all = new AllInOne("");
         CreateHomeObj obj = new CreateHomeObj();
 
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-        obj.setMerchantTradeDate( sdf.format(rentalOrder.getrentalOrdTime()) );
-        obj.setLogisticsSubType("TCAT");
-        obj.setGoodsAmount( String.valueOf( rentalOrder.getrentalAllPrice().add(rentalOrder.getrentalAllDepPrice()) ) );
-        obj.setSenderName( "howard" );
-        obj.setSenderCellPhone("0923561207");
+        obj.setMerchantTradeDate( sdf.format(rentalOrder.getrentalOrdTime()) ); // 交易時間
+        obj.setLogisticsSubType("TCAT"); // 物流子類型
+        obj.setGoodsAmount( String.valueOf( rentalOrder.getrentalAllPrice().add(rentalOrder.getrentalAllDepPrice()) ) ); // 交易金額
+        obj.setSenderName( "howard" ); // 寄件人姓名 未來代入管理員姓名
+        obj.setSenderCellPhone("0923561207"); // 寄件人手機號碼
         obj.setSenderZipCode("32041"); // 寄件人郵遞區號
         obj.setSenderAddress("32041桃園市中壢區延平路498號7樓之10"); // 寄件人地址
-        obj.setReceiverName( rentalOrder.getrentalRcvName() );
-        obj.setReceiverZipCode("32041");
-        obj.setReceiverCellPhone("0923561207");
-        obj.setReceiverAddress("32041桃園市中壢區復興路46號8樓");
-        obj.setScheduledPickupTime("4");
-        obj.setTemperature("0001");
-        obj.setDistance("00");
-        obj.setSpecification("0001");
+        obj.setReceiverName( rentalOrder.getrentalRcvName() ); // 收件人姓名
+        obj.setReceiverZipCode("32041"); // 收件人郵遞區號
+        obj.setReceiverCellPhone("0923561207"); // 收件人手機號碼
+        obj.setReceiverAddress("32041桃園市中壢區復興路46號8樓"); // 收件人地址
+        obj.setScheduledPickupTime("4"); // 預定收件時段
+        obj.setTemperature("0001"); // 配送溫度
+        obj.setDistance("00"); // 距離
+        obj.setSpecification("0001"); // 貨物規格
         obj.setServerReplyURL( "http://localhost:8080/backend/rentalorder/testToRentalCart" ); // 訂單狀態從這通知
 //        obj.setClientReplyURL( "http://localhost:8080/backend/rentalorder/testToRentalCart" );
         String formHTML = all.create(obj);
