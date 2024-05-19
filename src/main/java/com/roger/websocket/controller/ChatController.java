71168b7f2629f0db92e7ad36f58e1777f4de9629
zhengchangsun <a0967067696@gmail.com>
2024-05-19 10:00:51 +0800

    完善訊息通知

--- src/main/java/com/roger/websocket/controller/ChatController.java
+++ src/main/java/com/roger/websocket/controller/ChatController.java
@@ -1,5 +1,6 @@
 package com.roger.websocket.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.roger.websocket.model.ChatMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -7,6 +8,10 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class ChatController {
@@ -41,11 +46,35 @@ public class ChatController {
     @MessageMapping("/chat.sendImage")
     @SendTo("/topic/public")
     public ChatMessage sendImage(@Payload ChatMessage chatMessage) {
-        // 在這裡處理接收到的圖片數據，例如保存到文件系統或數據庫中
+
+        // 在你的類中定義一個 ObjectMapper 實例
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        if (chatMessage.getType() == ChatMessage.MessageType.IMAGE_AND_CHAT) {
+            try {
+                // 將 content 字符串轉換為 Map 物件
+                Map<String, Object> contentMap = objectMapper.readValue(chatMessage.getContent(), Map.class);
+
+                // 從 Map 對象中獲取文字和圖片資訊
+                String text = (String) contentMap.get("text");
+                String image = (String) contentMap.get("image");
+
+                // 創建一個包含文字和圖片資訊的 Map 物件並設置到 chatMessage.content 中
+                Map<String, String> messageContent = new HashMap<>();
+                messageContent.put("text", text);
+                messageContent.put("image", image);
+                chatMessage.setContent(String.valueOf(messageContent));
+            } catch (IOException e) {
+                // 處理 JSON 解析異常
+                e.printStackTrace();
+            }
+        }
+
+        if (chatMessage.getType() == ChatMessage.MessageType.IMAGE) {
+            // 在這裡處理接收到的圖片數據，例如保存到文件系統或數據庫中
+        }
 
         // 返回接收到的消息
         return chatMessage;
     }
-
-
 }
