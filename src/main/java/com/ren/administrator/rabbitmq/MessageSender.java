5429d5d93b90cc87ee25f3c3a1e50607843882f2
Larry <ciren22007524@gmail.com>
2024-05-20 10:14:02 +0800

    專案合併處理

--- src/main/java/com/ren/administrator/rabbitmq/MessageSender.java
+++ src/main/java/com/ren/administrator/rabbitmq/MessageSender.java
@@ -1,20 +1,20 @@
-//package com.ren.administrator.rabbitmq;
-//
-//import org.springframework.amqp.rabbit.core.RabbitTemplate;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class MessageSender {
-//
-//    private final RabbitTemplate rabbitTemplate;
-//
-//    @Autowired
-//    public MessageSender(RabbitTemplate rabbitTemplate) {
-//        this.rabbitTemplate = rabbitTemplate;
-//    }
-//
-//    public void sendMessage(String message) {
-//        rabbitTemplate.convertAndSend("exchangeName", "routingKey", message);
-//    }
-//}
+package com.ren.administrator.rabbitmq;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageSender {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    public MessageSender(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(String message) {
+        rabbitTemplate.convertAndSend("exchangeName", "routingKey", message);
+    }
+}
