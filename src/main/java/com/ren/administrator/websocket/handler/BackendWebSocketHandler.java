aa6165d0a3f461bb563bc962839b3c555c55eb50
iting <smart850811@gmail.com>
2024-05-22 13:59:53 +0800

    修到一半

--- src/main/java/com/ren/administrator/websocket/handler/BackendWebSocketHandler.java
+++ src/main/java/com/ren/administrator/websocket/handler/BackendWebSocketHandler.java
@@ -1,23 +1,23 @@
-//package com.ren.administrator.websocket;
-//
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.socket.TextMessage;
-//import org.springframework.web.socket.WebSocketSession;
-//import org.springframework.web.socket.handler.TextWebSocketHandler;
-//
-//@Component
-//public class BackendWebSocketHandler extends TextWebSocketHandler {
-//
-//    @Override
-//    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-//        // 處理接收到的WebSocket消息
-//        String payload = message.getPayload();
-//        System.out.println("Received: " + payload);
-//    }
-//
-//    @Override
-//    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-//        // 當WebSocket連接建立後
-//        System.out.println("WebSocket connection established");
-//    }
-//}
+package com.ren.administrator.websocket.handler;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class BackendWebSocketHandler extends TextWebSocketHandler {
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        // 處理接收到的WebSocket消息
+        String payload = message.getPayload();
+        System.out.println("Received: " + payload);
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        // 當WebSocket連接建立後
+        System.out.println("WebSocket connection established");
+    }
+}
