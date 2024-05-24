bcac0c62acc7b08cbcec276c9fa1ce5c72278480
Shirley Chang <sips81423@gmail.com>
2024-05-24 10:27:12 +0800

    add

--- src/main/java/com/yu/rentalmyfavorite/dto/AddToWishList.java
+++ src/main/java/com/yu/rentalmyfavorite/dto/AddToWishList.java
@@ -1,32 +1,37 @@
 package com.yu.rentalmyfavorite.dto;
 
-public class addToWishList {
+import org.springframework.data.redis.core.RedisHash;
 
-    private Integer rentalNo;
-    private Integer memNo;
-    private String rentalFavTime;
+    @RedisHash("AddToWishList")
+    public class AddToWishList {
 
-    public Integer getRentalNo() {
-        return rentalNo;
+        private Integer rentalNo;
+        private Integer memNo;
+        private String rentalFavTime;
+
+        public Integer getRentalNo() {
+            return rentalNo;
+        }
+
+        public void setRentalNo(Integer rentalNo) {
+            this.rentalNo = rentalNo;
+        }
+
+        public Integer getMemNo() {
+            return memNo;
+        }
+
+        public void setMemNo(Integer memNo) {
+            this.memNo = memNo;
+        }
+
+        public String getRentalFavTime() {
+            return rentalFavTime;
+        }
+
+        public void setRentalFavTime(String rentalFavTime) {
+            this.rentalFavTime = rentalFavTime;
+        }
+
     }
 
-    public void setRentalNo(Integer rentalNo) {
-        this.rentalNo = rentalNo;
-    }
-
-    public Integer getMemNo() {
-        return memNo;
-    }
-
-    public void setMemNo(Integer memNo) {
-        this.memNo = memNo;
-    }
-
-    public String getRentalFavTime() {
-        return rentalFavTime;
-    }
-
-    public void setRentalFavTime(String rentalFavTime) {
-        this.rentalFavTime = rentalFavTime;
-    }
-}
