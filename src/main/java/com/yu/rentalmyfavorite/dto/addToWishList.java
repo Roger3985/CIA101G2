0b4806c045b6148ce4c5386eb5a2be269c80702d
zhengchangsun <a0967067696@gmail.com>
2024-05-24 10:17:13 +0800

    錯誤

--- src/main/java/com/yu/rentalmyfavorite/dto/addToWishList.java
+++ src/main/java/com/yu/rentalmyfavorite/dto/addToWishList.java
@@ -1,32 +1,32 @@
-package com.yu.rentalmyfavorite.dto;
-
-public class addToWishList {
-
-    private Integer rentalNo;
-    private Integer memNo;
-    private String rentalFavTime;
-
-    public Integer getRentalNo() {
-        return rentalNo;
-    }
-
-    public void setRentalNo(Integer rentalNo) {
-        this.rentalNo = rentalNo;
-    }
-
-    public Integer getMemNo() {
-        return memNo;
-    }
-
-    public void setMemNo(Integer memNo) {
-        this.memNo = memNo;
-    }
-
-    public String getRentalFavTime() {
-        return rentalFavTime;
-    }
-
-    public void setRentalFavTime(String rentalFavTime) {
-        this.rentalFavTime = rentalFavTime;
-    }
-}
+//package com.yu.rentalmyfavorite.dto;
+//
+//public class addToWishList {
+//
+//    private Integer rentalNo;
+//    private Integer memNo;
+//    private String rentalFavTime;
+//
+//    public Integer getRentalNo() {
+//        return rentalNo;
+//    }
+//
+//    public void setRentalNo(Integer rentalNo) {
+//        this.rentalNo = rentalNo;
+//    }
+//
+//    public Integer getMemNo() {
+//        return memNo;
+//    }
+//
+//    public void setMemNo(Integer memNo) {
+//        this.memNo = memNo;
+//    }
+//
+//    public String getRentalFavTime() {
+//        return rentalFavTime;
+//    }
+//
+//    public void setRentalFavTime(String rentalFavTime) {
+//        this.rentalFavTime = rentalFavTime;
+//    }
+//}
