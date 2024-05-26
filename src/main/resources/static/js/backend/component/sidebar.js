c951e856eb7eb66f42db770b4715d760731d3018
Larry <ciren22007524@gmail.com>
2024-05-26 19:43:30 +0800

    後台網頁建置

--- src/main/resources/static/js/backend/component/sidebar.js
+++ src/main/resources/static/js/backend/component/sidebar.js
@@ -1,18 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // 查找所有具有 .dropright 類的元素
-    document.querySelectorAll('.dropright').forEach(function(dropright) {
-        dropright.addEventListener('mouseenter', function() {
-            this.querySelector('.dropdown-menu').style.display = 'block';
-            setTimeout(() => {
-                this.querySelector('.dropdown-menu').style.opacity = '1';
-            }, 0);
-        });
-
-        dropright.addEventListener('mouseleave', function() {
-            this.querySelector('.dropdown-menu').style.opacity = '0';
-            setTimeout(() => {
-                this.querySelector('.dropdown-menu').style.display = 'none';
-            }, 300);
-        });
+$(document).ready(function() {
+    $('.nav-item').hover(function() {
+        $(this).find('.collapse').collapse('show');
+    }, function() {
+        $(this).find('.collapse').collapse('hide');
     });
 });
